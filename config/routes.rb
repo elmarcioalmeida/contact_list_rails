1fb6b4fc3b06909d77c6efdc5832c132afdc0503
elmarcioalmeida <118493719+elmarcioalmeida@users.noreply.github.com>
2024-04-23 23:57:05 -0300

    Estudando

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
+  resources :contacts
+  resources :users, only: [:new, :create]
 
 end
